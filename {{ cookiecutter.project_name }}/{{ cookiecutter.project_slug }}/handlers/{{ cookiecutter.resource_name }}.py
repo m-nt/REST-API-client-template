e27ad82c4853f90ec4fe6102f039ed8eadd6456d
mohamad nosratian <68176509+m-nt@users.noreply.github.com>
2023-11-28 11:35:19 +0330

    Update {{ cookiecutter.resource_name }}.py

--- {{ cookiecutter.project_name }}/{{ cookiecutter.project_slug }}/handlers/{{ cookiecutter.resource_name }}.py
+++ {{ cookiecutter.project_name }}/{{ cookiecutter.project_slug }}/handlers/{{ cookiecutter.resource_name }}.py
@@ -9,33 +9,7 @@ from {{ cookiecutter.project_slug }}.models.{{ cookiecutter.resource_name }} imp
 
 HEADERS = {"accept": "application/json", "Content-Type": "application/json"}
 
-message_regex = r"(?<=message[\"']:[\"'])[a-zA-Z0-9 _\-]+|(?<=msg[\"']:[\"'])[a-zA-Z0-9 _\-]+|(?<=message[\"']: [\"'])[a-zA-Z0-9 _\-]+|(?<=msg[\"']: [\"'])[a-zA-Z0-9 _\-]+|(?<=detail[\"']:[\"'])[a-zA-Z0-9 _\-]+|(?<=detail[\"']: [\"'])[a-zA-Z0-9 _\-]+"
-
-
 class Handler:
-    @staticmethod
-    def get_response_message_v2(response: Response) -> Any:
-        matches = re.findall(message_regex, response.text)
-        if len(matches) > 0:
-            msg = [msg for msg in matches]
-            return {"message": ", ".join(msg[:])}
-        else:
-            if len(response.text) > 3 and isinstance(response.text, str):
-                return {"message": response.text}
-            return {"message": "Unknown error"}
-    @staticmethod
-    def get_response_message(response: Response) -> Any:
-        try:
-            data = response.json()
-        except Exception as e:
-            return {"message": "Unknown error"}
-        if "detail" in data:
-            return data["detail"]
-        if "content" in data:
-            return data["content"]
-        if "message" in data:
-            return data
-        
     @staticmethod
     async def _send_request(
         method: RequestMethod,
@@ -65,21 +39,11 @@ class Handler:
             json=json,
             headers=_headers,
         )
-        try:
-            async with httpx.AsyncClient(timeout=timeout) as client:
-                res: Response = await client.send(_request)
-        except Exception as error:
-            raise HTTPException(
-                status_code=500,
-                detail={
-                    "message": str(type(error))[8:-2],
-                },
-            )
-        if res.status_code != 200:
-            raise HTTPException(
-                status_code=res.status_code,
-                detail={
-                    "message": Handler.get_response_message_v2(res),
-                },
-            )
-        return res.json()
+        async with httpx.AsyncClient(timeout=timeout) as client:
+            res: Response = await client.send(_request)
+            if res.status_code != 200:
+                raise HTTPException(
+                    status_code=res.status_code,
+                    detail=res.json(),
+                )
+            return res.json()
